35b43876261c586b1faea26c363f7fdbcc37e1da
jyr <jyr@wavers.co.kr>
2022-06-12 12:15:27 +0900

    실습

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:row_and_column/screen/home_screen.dart';
+import 'package:row_and_column/screen/practice_screen.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: HomeScreen(),
+      home: PracticeScreen(),
     ),
   );
 }
---
+++ lib/screen/practice_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class PracticeScreen extends StatelessWidget {
+  const PracticeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: Container(
+          color: Colors.black,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    height: 50.0,
+                    width: 50.0,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    height: 50.0,
+                    width: 50.0,
+                    color: Colors.orange,
+                  ),
+                  Container(
+                    height: 50.0,
+                    width: 50.0,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    height: 50.0,
+                    width: 50.0,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+              Container(
+                height: 50.0,
+                width: 50.0,
+                color: Colors.orange,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    height: 50.0,
+                    width: 50.0,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    height: 50.0,
+                    width: 50.0,
+                    color: Colors.orange,
+                  ),
+                  Container(
+                    height: 50.0,
+                    width: 50.0,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    height: 50.0,
+                    width: 50.0,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+              Container(
+                height: 50.0,
+                width: 50.0,
+                color: Colors.green,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
