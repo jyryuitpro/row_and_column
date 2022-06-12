f7782d3997a83789d42a2659b8b80dfdcb31a156
jyr <jyr@wavers.co.kr>
2022-06-12 12:06:42 +0900

    Expanded 위젯과 Flexible 위젯

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -50,27 +50,46 @@ class HomeScreen extends StatelessWidget {
             // MainAxisSize - 주축 크기
             // max - 최대
             // min - 최소
-            mainAxisSize: MainAxisSize.min,
+            mainAxisSize: MainAxisSize.max,
             children: [
-              Container(
-                color: Colors.red,
-                width: 50.0,
-                height: 50.0,
+              // Expanded / Flexible
+              // 무조건 Row나 Column 위젯의 children에만 사용할 수 있다.
+
+              // Expanded - 최대한으로 남아있는 사이즈를 차지해라
+              // flex - 기본값 1, 나머지 공간을 나눠먹는 비율
+              // ex) 2 : 3 : 1 : 1
+
+              // Flexible - 비율만큼의 자리를 기본적으로 차지하지만 내부 위젯의 크기가 지정되어 있다면 그 크기만큼만 자리를 차지하고 남는공간은 버린다. (버려진 공간은 상위 위젯 (Column, Row)으로 귀속된다.)
+              // flex를 사용해서 비율을 정할 수 있다.
+              Flexible(
+                // flex: 2,
+                child: Container(
+                  color: Colors.red,
+                  width: 50.0,
+                  height: 50.0,
+                ),
               ),
-              Container(
-                color: Colors.orange,
-                width: 50.0,
-                height: 50.0,
+              Expanded(
+                // flex: 3,
+                child: Container(
+                  color: Colors.orange,
+                  width: 50.0,
+                  height: 50.0,
+                ),
               ),
-              Container(
-                color: Colors.yellow,
-                width: 50.0,
-                height: 50.0,
+              Expanded(
+                child: Container(
+                  color: Colors.yellow,
+                  width: 50.0,
+                  height: 50.0,
+                ),
               ),
-              Container(
-                color: Colors.green,
-                width: 50.0,
-                height: 50.0,
+              Expanded(
+                child: Container(
+                  color: Colors.green,
+                  width: 50.0,
+                  height: 50.0,
+                ),
               ),
             ],
           ),
